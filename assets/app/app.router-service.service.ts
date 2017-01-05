6af31ab7c3687c4c136680b27578465495258acb
Namone <al2xnord@yahoo.com>
2017-01-05 09:01:19 -0700

    Added route linking for sub pages in Auth page. Added routing service for use with User profile pages and other, more complex, routing.

--- assets/app/app.router-service.service.ts
+++ assets/app/app.router-service.service.ts
@@ -1,11 +1,14 @@
+import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
+import { User } from "./users/user.model";
 
+@Injectable()
 export class RouterService { 
     constructor(private router: Router) {} // get our Router object to use
 
     // This will redirect based on the user ID, etc. to certain user page
-    openUserPage(user) {
-        
+    openUserPage(user: User) {
+        this.router.navigateByUrl('/profile/${user.id}');
     }
 
 }
