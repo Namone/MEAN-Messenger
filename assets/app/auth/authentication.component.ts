3881ae5cac1f6d81ac37413252f9ae2d72d1c6a2
Namone <al2xnord@yahoo.com>
2017-01-05 08:08:54 -0700

    Adding sub links to Authentication page.

--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -3,8 +3,8 @@ import { Component } from "@angular/core";
 // TODO: create sub pages here for sign up, sign in, and log out. Verify user data is valid based on Angular models as well as Node models.
 @Component({
     selector: 'my-app-auth',
-    template: `<h1>Authentication!</h1>`
+    templateUrl: 'authentication.component.html'
 })
 export class AuthenticationComponent {
-
+    
 }
