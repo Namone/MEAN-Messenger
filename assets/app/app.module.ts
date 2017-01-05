a85e015208b512a938c23794958f234a587510a2
Alex Nordhausen <al2xnord@yahoo.com>
2017-01-04 19:43:52 -0700

    Routing work

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./shared/header.component";
@@ -8,6 +9,9 @@ import { MessageComponent } from "./messages/message.component";
 import { MessageListComponent } from "./messages/message-list.component";
 import { MessageInputComponent } from "./messages/message-input.component";
 
+const appRoutes: Routes = [
+    { path: '', component: MessageListComponent },
+]
 
 @NgModule({
     declarations: [
@@ -17,7 +21,7 @@ import { MessageInputComponent } from "./messages/message-input.component";
         MessageListComponent,
         MessageInputComponent
     ],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
     bootstrap: [AppComponent]
 })
 export class AppModule {
