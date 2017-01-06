a11496b9aa1c7a4e01cec527611cbf1c56a23e8a
Namone <al2xnord@yahoo.com>
2017-01-06 13:20:32 -0700

    Added router redirect. Signup - > singin, after submit.

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { AuthenticationService } from "./auth.service";
 import { NgForm } from "@angular/forms";
 import { User } from "../users/user.model";
+import { Router } from "@angular/router";
 
 @Component ({
     selector: 'app-signup',
@@ -9,7 +10,7 @@ import { User } from "../users/user.model";
 })
 export class SignupComponent {
 
-    constructor(private auth: AuthenticationService) { } // get our service object
+    constructor(private auth: AuthenticationService, private router: Router) { } // get our service object
 
     onSubmit(form: NgForm) {
         // Get my form fields
@@ -21,11 +22,12 @@ export class SignupComponent {
 
         // ... now do something with them
         this.auth.addUser(user)
-            .subscribe(
-                data => console.log(data)
-            );
+            .subscribe();
 
         form.reset();
+
+        // Redirect to sign in page...
+        this.router.navigate(['/auth/signin']);
     }
 
 }
